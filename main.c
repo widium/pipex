06fd4d78b4e5506c2d87881779ea0b0d88dcb6d5
Widium <eddiebennacer@orange.fr>
2022-06-16 19:45:42 +0200

    patch leak finish norminette

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ebennace <ebennace@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 09:56:51 by ebennace          #+#    #+#             */
-/*   Updated: 2022/06/16 18:23:43 by ebennace         ###   ########.fr       */
+/*   Updated: 2022/06/16 19:45:27 by ebennace         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 int	main(int argc, char **argv, char **env_path)
 {
-	terminal(argv, argc, env_path, 1);
+	terminal(argv, argc, env_path, 0);
 }
